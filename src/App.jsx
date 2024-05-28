b5038b0f784cbcd184ca35811f17da2f63ae7ec2
laychan25 <lauriesilva0@gmail.com>
2024-05-28 16:44:59 -0300

    postando projeto

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Carrinho from "./pages/Carrinho";
+import PaginaErro from "./pages/PaginaErro";
+
+import "./App.css";
+import { CarrinhoProvider } from "./context/carrinhoDeCompras";
+
+function App() {
+  return (
+    <BrowserRouter>
+    <CarrinhoProvider >
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/carrinho" element={<Carrinho />} />
+        <Route path="*" element={<PaginaErro />} />
+      </Routes>
+    </CarrinhoProvider>
+      
+    </BrowserRouter>
+  );
+}
+
+export default App;
